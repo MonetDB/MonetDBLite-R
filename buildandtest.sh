1d074e731aa714637278fc3d5d7f6ab6efc431df
Hannes Muehleisen <hannes@muehleisen.org>
2017-09-12 13:39:15 +0200

    mc for appveyor

---
+++ buildandtest.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+export MAKEFLAGS="-j"
+export MONETDBLITE_DEBUG=42
+rm MonetDBLite_*
+git submodule update --recursive --remote --depth 1 && \
+R CMD build . && \
+R CMD INSTALL --build MonetDBLite_*.tar.gz && \
+R CMD INSTALL MonetDBLite_*.tgz && \
+(cd tests; R  --slave -f testthat.R) && \
+(cd ~/source/dbplyr/tests && git pull && R --slave -f testthat.R)
