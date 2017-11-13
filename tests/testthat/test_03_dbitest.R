873fa7bd8d411e252b0626b79e6370ac497359d2
Hannes Muehleisen <hannes@muehleisen.org>
2017-11-13 11:44:47 +0100

    no dbitest in cran checks

--- tests/testthat/test_03_dbitest.R
+++ tests/testthat/test_03_dbitest.R
@@ -1,3 +1,4 @@
+if (Sys.getenv("NOT_CRAN", unset="no") != "no") {
 if (Sys.getenv("MONETDBLITE_INMEMORY", unset="no") == "yes") {
 	dbfolder <- ":memory:"
 } else {
@@ -19,10 +20,8 @@ DBItest::make_context(MonetDBLite::MonetDBLite(), dbfolder, tweaks=DBItest::twea
 
 skip <- c("package_name", "send_query_only_one_result_set", "send_statement_only_one_result_set", "data_64_bit_numeric_warning", "data_64_bit_lossless", "quote_string_na_is_null", "roundtrip_64_bit_numeric", "roundtrip_64_bit_character", "roundtrip_time", "roundtrip_timestamp", "roundtrip_field_types", "get_info_result", "overwrite_table_missing", "compliance", "get_query_n_bad", "get_query_good_after_bad_n", "data_timestamp", "read_table_row_names_false", "read_table_row_names_default", "roundtrip_numeric_special", "cannot_forget_disconnect")
 
-testthat::skip_on_cran()
 DBItest::test_all(skip=skip)
 
-
 MonetDBLite::monetdblite_shutdown()
-
 warnings()
+}
