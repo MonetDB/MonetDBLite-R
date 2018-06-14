e6bc0b941c37ed338197a0fda32f360fb1ac8b9c
Hannes Muehleisen <hannes@muehleisen.org>
2018-06-14 18:27:25 +0200

    update update script

--- buildandtest.sh
+++ buildandtest.sh
@@ -4,9 +4,9 @@ export MAKEFLAGS=-j
 export CFLAGS="-O0 -g -Wall -Wextra -Werror -Wmissing-prototypes -Wold-style-definition -Wpedantic"
 
 rm MonetDBLite_*
-git submodule update --recursive --remote --depth 1 && \
+ git fetch monetdblite && git subtree pull --prefix src/monetdblite monetdblite mar --squash && \
 R CMD build . && \
 R CMD INSTALL --build MonetDBLite_*.tar.gz && \
 R CMD INSTALL MonetDBLite_*.tgz && \
-(cd tests; R  --slave -f testthat.R) && \
-(cd ~/source/dbplyr/tests && git pull && R --slave -f testthat.R)
+(cd tests; R  --slave -f testthat.R) # && \
+#(cd ~/source/dbplyr/tests && git pull && R --slave -f testthat.R)
