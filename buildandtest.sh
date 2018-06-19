528394c79410e1a9ecf3c6cd3a2c914adc03234f
Hannes Muehleisen <hannes@muehleisen.org>
2018-06-19 10:32:13 +0200

    changing branch to pull from

--- buildandtest.sh
+++ buildandtest.sh
@@ -4,7 +4,7 @@ export MAKEFLAGS=-j
 export CFLAGS="-O0 -g -Wall -Wextra -Werror -Wmissing-prototypes -Wold-style-definition -Wpedantic"
 
 rm MonetDBLite_*
- git fetch monetdblite && git subtree pull --prefix src/monetdblite monetdblite mar --squash && \
+ git fetch monetdblite && git subtree pull --prefix src/monetdblite monetdblite master --squash && \
 R CMD build . && \
 R CMD INSTALL --build MonetDBLite_*.tar.gz && \
 R CMD INSTALL MonetDBLite_*.tgz && \
