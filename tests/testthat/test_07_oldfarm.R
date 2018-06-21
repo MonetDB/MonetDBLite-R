3c49f3b44af003385b684e2cca54d596f582fb81
Hannes Muehleisen <hannes@muehleisen.org>
2018-06-21 16:30:32 +0200

    upgrade tests, test that databases created with older versions of monetdblite for r are still readable with new version

---
+++ tests/testthat/test_07_oldfarm.R
@@ -0,0 +1,33 @@
+library(testthat)
+library(DBI)
+
+versions <- c("0.5.0", "0.5.1")
+
+test_that("we can upgrade old dbs", {
+	for (v in versions) {
+		message(v)
+		dbfile <- sprintf("oldfarm-%s.zip", v)
+		uf <- file.path(tempdir(), "upgradetestfarm")
+		unzip(dbfile, exdir=uf)
+		dbfolder <- list.files(uf, full.names=T)[[1]]
+		con <- dbConnect(MonetDBLite::MonetDBLite(), dbfolder)
+		expect_true(dbExistsTable(con, "iris"))
+		res <- dbReadTable(con, "iris")
+		res$Species <- as.factor(res$Species)
+		expect_equal(res, iris)
+		dbDisconnect(con, shutdown=T)
+		# start again because it might crash on shutdown
+		con <- dbConnect(MonetDBLite::MonetDBLite(), dbfolder)
+		dbDisconnect(con, shutdown=T)
+	}
+})
+
+create <- function(v) {
+	devtools::install_version("MonetDBLite", version = v, repos = "https://cloud.r-project.org/")
+	con <- dbConnect(MonetDBLite::MonetDBLite(), v)
+	dbWriteTable(con, "iris", iris)
+	dbDisconnect(con, shutdown=T)
+
+	dbfile <- sprintf("oldfarm-%s.zip", v)
+	zip(dbfile, v)
+}
